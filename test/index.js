405d344ab49ae95e5cd2727ef5e4b2bf1b2b63d4
Jannis R <mail@jannisr.de>
2021-09-06 17:07:14 +0200

    set isParseError: true on parsing errors; 2.1.1

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const {inspect} = require('util')
-const {deepStrictEqual} = require('assert')
+const {deepStrictEqual, strictEqual, fail} = require('assert')
 const lexer = require('../lib/lexer')
 const parse = require('..')
 const cases = require('./cases')
@@ -18,5 +18,16 @@ for (const [id, name, expected] of cases) {
 	console.log(inspect(res, {depth: null, colors: true})) // todo: indent
 	if (expected) deepStrictEqual(res, expected)
 }
+
+(() => {
+	try {
+		parse('foo (bar) (baz)')
+	} catch (err) {
+		strictEqual(err.isParseError, true, `parse error's isParseError is invalid`)
+		strictEqual(err.input, 'foo (bar) (baz)', `parse error's input is invalid`)
+		return;
+	}
+	fail(`parsing "foo (bar) (baz)" didn't throw an error`)
+})()
 
 console.info('\n✔︎ all test cases passed')
